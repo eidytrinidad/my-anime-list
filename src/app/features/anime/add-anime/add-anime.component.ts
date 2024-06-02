356abdfbbd9ebc76b3074db44a9255f189a09dbd
Eidy <eidyjtf@gmail.com>
2024-06-02 17:48:53 -0400

    start greating form structure in add anime component

--- src/app/features/anime/add-anime/add-anime.component.ts
+++ src/app/features/anime/add-anime/add-anime.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AnimeState } from 'src/app/core/constants/anime';
+import { AnimeService } from 'src/app/core/services/anime.service';
 
 @Component({
   selector: 'app-add-anime',
   templateUrl: './add-anime.component.html',
-  styleUrls: ['./add-anime.component.css']
+  styleUrls: ['./add-anime.component.css'],
 })
 export class AddAnimeComponent implements OnInit {
+  public animeForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private animeService: AnimeService
+  ) {
+    this.animeForm = this.formBuilder.group({
+      id: [0],
+      imgUrl: ['', Validators.required],
+      title: ['', Validators.required],
+      state: [AnimeState.ENABLE, Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
 }
