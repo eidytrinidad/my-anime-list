5a94e2cbe8e8b4c295435a9b3c604af8fbfda4cc
Eidy <eidyjtf@gmail.com>
2024-06-02 16:40:36 -0400

    styling anime list and card component, changed bg to animelist logo

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimeComponent, AnimeListComponent } from './anime';
+import { SharedModule } from '../shared/shared.module';
 
 const declarations = [AnimeComponent, AnimeListComponent];
 @NgModule({
   declarations: [...declarations],
-  imports: [CommonModule],
+  imports: [CommonModule,SharedModule],
   exports: [...declarations],
 })
 export class FeaturesModule {}
