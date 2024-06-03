cf2d0dd1e880066a0d92311761cd4cddc4de0161
Eidy <eidyjtf@gmail.com>
2024-06-03 10:10:15 -0400

    add routermodule to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent, HeaderComponent, FormComponent } from './components/';
+import { RouterModule } from '@angular/router';
 
 const declarations = [HeaderComponent, CardComponent, FormComponent];
 
 @NgModule({
   declarations: [...declarations],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [...declarations],
 })
 export class SharedModule {}
